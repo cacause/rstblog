f83b4bfc2f0a278b73224a0e8e88a17fae164cce
Yax <kianby@madyanne.fr>
2013-05-09 20:23:18 +0200

    Improve cacause module. - module is linked to before_template_rendered signal - comments meta are loaded in memory but comments body are parsed on-demand - rest markup is converted to HTML fragment - template context is enriched with related comments

--- rstblog/modules/cacause.py
+++ rstblog/modules/cacause.py
@@ -12,8 +12,9 @@ import sys
 import os
 import yaml
 import hashlib
-from rstblog.signals import before_file_built
+from rstblog.signals import before_template_rendered
 from StringIO import StringIO
+from docutils import core
 import jinja2
 
 cacause_context = {}
@@ -25,15 +26,31 @@ def get_article_id(source_filename):
     return m.hexdigest()
 
 
-def get_comment_meta(comment_file):
-    headers = ['---']
+def read_comment(comment_file, header=True):
+    content = ['---']
     f = open(comment_file, 'r')
+
+    # read comment header
     for line in f:
         line = line.rstrip()
         if not line:
             break
-        headers.append(line)
+        content.append(line)
 
+    # read comment body
+    if not header:
+        content = []
+        for line in f:
+            line = line.rstrip()
+            content.append(line.decode('utf-8'))
+    f.close()
+
+    # return header or body
+    return content
+
+
+def get_comment_meta(comment_file):
+    headers = read_comment(comment_file)
     cfg = yaml.load(StringIO('\n'.join(headers)))
     if cfg:
         if not isinstance(cfg, dict):
@@ -54,45 +71,39 @@ def read_comments(comment_dir):
             if not article_id in cacause_context['comments']:
                 cacause_context['comments'][article_id] = []
             cacause_context['comments'][article_id].append(cfg)
-    print "### cacause context"
-    print cacause_context
 
 
-def process_blog_entry(context):
-    # create cacause context is empty
+def rest_to_html_fragment(a_str):
+    parts = core.publish_parts(
+        source=a_str,
+        writer_name='html')
+    fragment = parts['body_pre_docinfo'] + parts['fragment']
+    return jinja2.Markup(fragment.encode('utf-8'))
+
+
+def enhance_template_context(template, context):
+    if not 'ctx' in context:
+        return
+
+    # create cacause context if empty
     if not 'comments' in cacause_context:
-        template_context = context.get_default_template_context()
-        cacause_dir = template_context['config'].get('cacause_dir')
+        cacause_dir = context['config'].get('cacause_dir')
         if not cacause_dir:
             print "cacause: can't find cacause_dir in config.yml"
             sys.exit(1)
-        read_comments("/".join([context.builder.project_folder, cacause_dir]))
+        read_comments("/".join(
+            [context['builder'].project_folder, cacause_dir]))
 
-
-@jinja2.contextfunction
-def get_cacause(context):
-    print "###### JINJA CACAUSE ###"
-    print cacause_context
-    print context
-
+    # add related comments to template context if any
+    context['ctx'].comments = []
     article_id = get_article_id(context['source_filename'])
-    count = 0
     if article_id in cacause_context['comments']:
-        count = len(cacause_context['comments'][article_id])
-    print "%d comment(s)" % count
+        for comment_meta in cacause_context['comments'][article_id]:
+            comment_body = read_comment(comment_meta['comment'], False)
+            comment = comment_meta.copy()
+            comment['body'] = rest_to_html_fragment('\n'.join(comment_body))
+            context['ctx'].comments.append(comment)
 
-    # TODO: insert existing comments here
-
-    # TODO insert comment creation form here
-    cacause_txt="""
-<div id="cacause_thread">
-<p>insert %d existing comment(s) here<p>
-<p>insert creation comment form here</p>
-</div>
-""" % count
-
-    return jinja2.Markup(cacause_txt.encode('utf-8'))
 
 def setup(builder):
-    before_file_built.connect(process_blog_entry)
-    builder.jinja_env.globals['get_cacause'] = get_cacause
+    before_template_rendered.connect(enhance_template_context)
